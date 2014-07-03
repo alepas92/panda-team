64f775c507bad06953a33f820bab92d8bf540910
Bychyk <marichkabychyk@gmail.com>
2014-07-03 16:10:28 +0300

    Controller commit

---
+++ dev/js/app/controllers/controllers.js
@@ -0,0 +1,100 @@
+
+//New Outlay/Income    Daily
+function readDailyOutlays(){
+	var category = $( '#new-outlay select.sel-cat' ).val(),
+		cost = $('#new-outlay .inp-cos').val();
+	data.setDaily(todayKeyWord, category, cost, 'outlays' );
+}
+
+$('#new-outlay .btn-submit').click(function() {	
+	
+	readDailyOutlays();
+});
+
+function readDailyIncomes(){
+	var category = $( '#new-income select.sel-cat' ).val(),
+		cost = $('#new-income .inp-cos').val();
+	data.setDaily(todayKeyWord, category, cost, 'incomes' );
+}
+
+$('#new-income .btn-submit').click(function() {	
+	alert('click');
+	readDailyIncomes();
+});
+
+
+//Daily Categories outlays/income
+
+function readCategoryOutlays(){
+	var category = $( '#new-outlay .inp-cat' ).val();
+	data.setCategory(category, 'outlays' );
+}
+
+$('#new-outlay .btn-add').click(function() {	
+	
+	readCategoryOutlays();
+});
+
+
+
+function readCategoryIncomes(){
+	var category = $( '#new-income .inp-cat' ).val();
+	data.setCategory(category, 'incomes' );
+}
+
+$('#new-income .btn-add').click(function() {	
+	alert('click');
+	readCategoryIncomes();
+});
+
+
+//Regular day,Outlay
+/*	var category = $( '#regular-outlay select.sel-cat' ).val(),
+		cost = $('#regular-outlay .inp-cos').val();
+		//time = $('#regular-outlay .inp-dat').val();
+	//data.setRegular(category, cost, 'day', 'outlays' );
+}
+
+$('#regular-outlay .btn-submit').click(function() {	
+	alert('click');
+	readRegularOutlays();
+});
+
+*/
+
+//Manage forecast(month, outlays)
+function readForecastOutlays(){
+	var category = $( '#manage-forecast select.sel-cat' ).val();
+	var cost = $('#manage-forecast .inp-cos').val();
+	data.setForecast(category, cost, 'month', 'outlays' );
+}
+
+$('#manage-forecast .btn-submit').click(function() {	
+	alert('click');
+	readForecastOutlays();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Regular Income
+
+
+//Manage balance
+//Statistics
