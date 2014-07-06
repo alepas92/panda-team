307a81b48eecb9f23a0d315d0ef1524aa74cc3e8
Gorgonello <andriygavriluk@gmail.com>
2014-07-06 22:18:54 +0300

    It's syple non-styled incomes-outlays  manage

--- build/js/script.js
+++ build/js/script.js
@@ -320,7 +320,13 @@ $(function(){
 	var balance=dataTotalStat.totalStatistic['currentBalance'];
 	if(balance!==null)
 		$('#curBalance').html('Your current balance:' +balance);
-});function LS () {}
+});$('#manage-outlay-painting').click(function() {
+	managePanelPainting('outlays');
+});
+
+$('#manage-incomes-painting').click(function() {
+	managePanelPainting('incomes');
+});;function LS () {}
 
 LS.get = function (keyWord) {
 	if (localStorage[keyWord] !== null && localStorage[keyWord] !== undefined ) {
@@ -1234,41 +1240,164 @@ function appearInfoBlock() {
 	} else {
 		leftNavBar.style.display = "none";
 	}
-};managePanelPainting('outlays');
-managePanelPainting('incomes');
-
-function managePanelPainting (type) {
+};function managePanelPainting (type) {
 
 	if (type === 'outlays') {
-		var placeholder = document.getElementById('manage-outlay'),
-			fieldset;
 		
 		for (var key in dataDayly[todayKeyWord].outlays) {
-			fieldset = document.createElement('fieldset');
-			fieldset.className = 'manage-panel-fieldset';
-
-			placeholder.appendChild(fieldset);
+			createInputsField('manage-outlay', key, dataDayly[todayKeyWord].outlays[key].cat, dataDayly[todayKeyWord].outlays[key].cost, 'Outlay');
 		}
+
+		var button = createButton('submit-edited-outlays', '', 'Submit');
+		document.getElementById('manage-outlay').appendChild(button);
 
 	} else if ( type === 'incomes' ) {
-		var placeholder = document.getElementById('manage-income'),
-			fieldset;
 
 		for (var key in dataDayly[todayKeyWord].incomes) {
-			fieldset = document.createElement('fieldset');
-			fieldset.className = 'manage-panel-fieldset';
-
-			placeholder.appendChild(fieldset);
+			createInputsField('manage-income', key, dataDayly[todayKeyWord].incomes[key].cat, dataDayly[todayKeyWord].incomes[key].cost, 'Income');
 		}
+
+		var button = createButton('submit-edited-outlays', '', 'Submit');
+		document.getElementById('manage-outlay').appendChild(button);
+
+	} else {
+		console.log('error(outlayManagePainting.js)');
 	}
 }
 
-	function createInputField () {
-		createLable();
+	function createInputsField (placeholderId, key, categoryValue, costValue, legendTitle) {
+		var fieldset, categoryInput, costInput, deleteButton;
+		var placeholder = document.getElementById(placeholderId);
+
+		fieldset = createFieldset(key, legendTitle);
+		categoryInput = createInputLabel('Category: ', categoryValue);
+		costInput = createInputLabel('Cost: ', costValue);
+		deleteButton = createButton(key, 'manage-delete-button', 'delete');
+
+		placeholder.appendChild(fieldset).appendChild(categoryInput);
+		placeholder.appendChild(fieldset).appendChild(costInput);
+		placeholder.appendChild(fieldset).appendChild(deleteButton);
+
 	}
 
-		function createLable () {
-			var label = document.createElement( 'label' );
+	function createFieldset (id, legendValue) {
+		var fieldset = document.createElement('fieldset');
+		fieldset.className = 'manage-panel-fieldset';
+		fieldset.id = id;
+
+		var legend = document.createElement('legend');
+		legend.innerHTML = legendValue;
+
+		fieldset.appendChild(legend); 
+		
+		return fieldset
+	}
+
+	function createInputLabel (labelValue, inputValue) {
+		var label = createLable(labelValue),
+		input = document.createElement('input');
+		
+		input.type = 'text';
+		input.value = inputValue;
+
+		label.appendChild(input);
+
+		return label;
+	}
+
+	function createButton (id, buttonClass, value) {
+		var button = document.createElement('input');
+
+		button.type = 'button';
+		button.id = id;
+		button.className = buttonClass;
+		button.value = value;
+
+		return button
+	}
+
+		function createLable (text) {
+			var label = document.createElement('label');
+			label.textContent = text;
+
+			return label
+		};function managePanelPainting (type) {
+	if (type === 'outlays') {
+		
+		for (var key in dataDayly[todayKeyWord].outlays) {
+			createInputsField('manage-outlay', key, dataDayly[todayKeyWord].outlays[key].cat, dataDayly[todayKeyWord].outlays[key].cost, 'Outlay');
+		}
+
+		var button = createButton('submit-edited-outlays', '', 'Submit');
+		document.getElementById('manage-outlay').appendChild(button);
+
+	} else if ( type === 'incomes' ) {
+
+		for (var key in dataDayly[todayKeyWord].incomes) {
+			createInputsField('manage-income', key, dataDayly[todayKeyWord].incomes[key].cat, dataDayly[todayKeyWord].incomes[key].cost, 'Income');
+		}
+
+		var button = createButton('submit-edited-outlays', '', 'Submit');
+		document.getElementById('manage-income').appendChild(button);
+
+	}
+}
+
+	function createInputsField (placeholderId, key, categoryValue, costValue, legendTitle) {
+		var fieldset, categoryInput, costInput, deleteButton;
+		var placeholder = document.getElementById(placeholderId);
+
+		fieldset = createFieldset(key, legendTitle);
+		categoryInput = createInputLabel('Category: ', categoryValue);
+		costInput = createInputLabel('Cost: ', costValue);
+		deleteButton = createButton(key, 'manage-delete-button', 'delete');
+
+		placeholder.appendChild(fieldset).appendChild(categoryInput);
+		placeholder.appendChild(fieldset).appendChild(costInput);
+		placeholder.appendChild(fieldset).appendChild(deleteButton);
+
+	}
+
+	function createFieldset (id, legendValue) {
+		var fieldset = document.createElement('fieldset');
+		fieldset.className = 'manage-panel-fieldset';
+		fieldset.id = id;
+
+		var legend = document.createElement('legend');
+		legend.innerHTML = legendValue;
+
+		fieldset.appendChild(legend); 
+		
+		return fieldset
+	}
+
+	function createInputLabel (labelValue, inputValue) {
+		var label = createLable(labelValue),
+		input = document.createElement('input');
+		
+		input.type = 'text';
+		input.value = inputValue;
+
+		label.appendChild(input);
+
+		return label;
+	}
+
+	function createButton (id, buttonClass, value) {
+		var button = document.createElement('input');
+
+		button.type = 'button';
+		button.id = id;
+		button.className = buttonClass;
+		button.value = value;
+
+		return button
+	}
+
+		function createLable (text) {
+			var label = document.createElement('label');
+			label.textContent = text;
+
 			return label
 		};$(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
---
+++ dev/js/app/controllers/managePanelControllers.js
@@ -0,0 +1,7 @@
+$('#manage-outlay-painting').click(function() {
+	managePanelPainting('outlays');
+});
+
+$('#manage-incomes-painting').click(function() {
+	managePanelPainting('incomes');
+});
