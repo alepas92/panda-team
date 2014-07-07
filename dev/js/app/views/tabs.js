c64a7d8812b7cdc758a7007feb084bbd36f1b1f1
Gorgonello <andriygavriluk@gmail.com>
2014-07-08 02:28:02 +0300

    Some try to make normal tools panel

--- dev/js/app/views/tabs.js
+++ dev/js/app/views/tabs.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
         var currentAttrValue = $(this).attr('href');
- 
-        // Show/Hide Tabs
-        $('.tabs ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
-        $(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
+ 		
+ 		console.log();
+
+
+        if ($(this).attr('href') === "#empty") {
+        } else if ($(this).parents('li:eq(1)').attr('id') === 'dropdown-item') {
+        	$('.tabs ' + currentAttrValue).show().siblings().hide();
+ 			$(this).parents('li:eq(1)').addClass('active').siblings().removeClass('active');
+ 			e.preventDefault();
+ 		} else {
+ 			$('.tabs ' + currentAttrValue).show().siblings().hide();
+	        $(this).parent('li').addClass('active').siblings().removeClass('active');
+	        e.preventDefault();
+ 		}
     });
 });
